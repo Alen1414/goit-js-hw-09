9f105d803ac7c79506f6eae73054a59fd8e1c0d5
Alona Puhach <alen1414@gmail.com>
2022-01-26 15:09:39 -0400

    ups

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,7 +13,7 @@ function onBtn(e) {
   let amount = Number(e.currentTarget.amount.value);
   
 
-  for (let position = 0; position === amount; position += 1)
+  for (let position = 1; position <= amount; position += 1)
   {
     createPromise(position, delay)
       .then(({ position,delay }) => {
@@ -26,27 +26,36 @@ function onBtn(e) {
            Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
         },delay)
       });
-    // delay += step;
+    delay += step;
   }
 }
 
+
+  // setInterval(() => {
+  //   if (position >= amount) {
+  //     return;
+  //   }
+  //   position += 1;
+  //   setTimeout(() => {
+  //     delay += step;
+  //   });
    
 function createPromise(position, delay) {
     const shouldResolve = Math.random() > 0.3;
  
     
     return new Promise((resolve, reject) => {
-     
       if (shouldResolve) {
         resolve({position,delay})
       } else {
         reject({position,delay})
       }
     })
-    
 }
-createPromise( )
-// if (Math.round(delayTime / 1000) === 0) {
-//   console.log('Отсчет окончен');
-//   return;
-// }
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
